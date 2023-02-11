8e2c1bbb8566912a3cfaa8c0cc0570952dd73af6
harsha <manyamharsha112@gmail.com>
2023-02-10 20:32:30 -0600

    updated resume

--- src/components/Projects/projectdata.js
+++ src/components/Projects/projectdata.js
@@ -1,24 +1,10 @@
 const projects = [
     {
-        title: "E-commerce Store",
-        image: `https://user-images.githubusercontent.com/29236686/162415844-82332478-77d4-4b44-ae7d-f7b9ff49abb2.svg`,
-        description: "Its an E-commerce application. We can view each items and can add to the cart. It have search and pagination features. Added Razor pay payment option after checking out the items",
-        link: "https://super-smart-store.herokuapp.com/",
-        source: "https://github.com/Harsha70/Super-store",
-    }, 
-    {
         title: "Movie App",
         image: `https://user-images.githubusercontent.com/29236686/162419160-198a94d6-88ae-460e-b2a5-0e05c71cc997.svg`,
         description: "It's a Movie application based on the public movie apis. We can bookmark a movie to watch it later",
         link: "https://movie-app123.netlify.app/",
         source: "https://github.com/Harsha70/Movieapp",
-    },
-    {
-        title: "Event-Calendar",
-        image: `https://user-images.githubusercontent.com/29236686/162422716-d6fe4a2a-2749-4d92-a935-6f3bece26180.svg`,
-        description: "It is a Calendar build with google calendar api. We can add, edit and delete the event from the calendar",
-        link: "https://github.com/Harsha70/InventamTechCalendar",
-        source: "https://github.com/Harsha70/InventamTechCalendar",
     },
     {
         title: "Flickgram",
@@ -28,18 +14,32 @@ const projects = [
         source: "https://github.com/Harsha70/Flickgram",
     },
     {
-        title: "Notes",
-        image: `https://user-images.githubusercontent.com/29236686/162422784-9035291f-a75a-4d43-9b7a-37817db9b58e.svg`,
-        description: "It is a note making application with text editor. Firebase is used for storing respective new text notes",
-        link: "https://ever-noteclone.netlify.app/",
-        source: "https://github.com/Harsha70/EverNote",
-    },
-    {
         title: "Chat Application",
         image: `https://user-images.githubusercontent.com/29236686/162422873-fdf62fda-1fea-46bd-b52a-f43f0130db8d.svg`,
         description: "Its a simple chat-application. it has google authentication to sign in. We can add new groups and edit messages and upload images in the chat.",
         link: "https://public-group-chat.netlify.app/",
         source: "https://github.com/Harsha70/Chatapplication",
+    },
+    {
+        title: "E-commerce Store",
+        image: `https://user-images.githubusercontent.com/29236686/162415844-82332478-77d4-4b44-ae7d-f7b9ff49abb2.svg`,
+        description: "Its an E-commerce application. We can view each items and can add to the cart. It have search and pagination features. Added Razor pay payment option after checking out the items",
+        link: "https://github.com/Harsha70/Super-store",
+        source: "https://github.com/Harsha70/Super-store",
+    }, 
+    {
+        title: "Event-Calendar",
+        image: `https://user-images.githubusercontent.com/29236686/162422716-d6fe4a2a-2749-4d92-a935-6f3bece26180.svg`,
+        description: "It is a Calendar build with google calendar api. We can add, edit and delete the event from the calendar",
+        link: "https://github.com/Harsha70/InventamTechCalendar",
+        source: "https://github.com/Harsha70/InventamTechCalendar",
+    },
+    {
+        title: "Notes",
+        image: `https://user-images.githubusercontent.com/29236686/162422784-9035291f-a75a-4d43-9b7a-37817db9b58e.svg`,
+        description: "It is a note making application with text editor. Firebase is used for storing respective new text notes",
+        link: "https://github.com/Harsha70/EverNote",
+        source: "https://github.com/Harsha70/EverNote",
     },
 ]
 
